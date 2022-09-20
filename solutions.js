969f8e6898319dbc6a40b10e47478f6ac0697ab6
designhappily <65686852+shivakalai@users.noreply.github.com>
2022-09-20 16:02:12 +0200

    event listener problem solved

--- solutions.js
+++ solutions.js
@@ -28,3 +28,37 @@ newTopic.textContent = "My New FAQ"; //<section class="parent">My New FAQ</secti
 nav.appendChild(newTopic);
 
 ​
+/* 
+4. Webiste Name: OnePlus
+
+Topic:  Query Selector, InnerText
+Tasks: Change the contact number
+ */
+
+let num = document.querySelector(".one-tel-number");
+num.textContent = "+91-9791032330";
+
+
+/* 
+5. Webiste Name: Samsung
+
+Topic:  getElementById, createElement, InnerText, append, setAttribute
+Task: Target the main div of card and change the Button text to Check out
+*/
+let btn = document.querySelector(".diwali-deals-product-sale-btn").innerHTML;
+let newBtn = document.createElement("button");
+newBtn.className = "diwali-deals-product-sale-btn";
+newBtn.appendChild(document.createElement("h3"));
+newBtn.textContent = "Check Out";​
+//or below way
+document.querySelector(".diwali-deals-product-sale-btn").innerHTML = "Check out";
+
+
+//6.Webiste Name: Adidas
+/* Topic:  Query Selector, Event listeners, Changing Styles
+Task: Target the search box and on hover change thebackground color to red.
+ */
+let search = document.querySelector(".searchinput___19uW0");
+search.addEventListener("mouseover", function() {
+    document.querySelector(".searchinput___19uW0").style.backgroundColor = "green";
+});
